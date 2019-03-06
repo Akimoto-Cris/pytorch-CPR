88bef47180cd1393f5e61a20a3d9d4c6780ef47c
Akimoto-Cris <29163135+Akimoto-Cris@users.noreply.github.com>
2019-03-06 14:34:11 +0800

    fix test time memory leak

--- src/training/train_net.py
+++ src/training/train_net.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 from visualization.visualize import visualize_output
 
 
+# torch.backends.cudnn.enabled = False
+# torch.backends.cudnn.benchmark = False
+
 class process:
     def __init__(self, model):
         self.model = model
@@ -32,14 +35,15 @@ class process:
                 for i in range(len(heatmap_outputs)):
                     heatmap_out = heatmap_outputs[i]
                     paf_out = paf_outputs[i]
-                    loss_hm_total += criterion_hm(heatmap_out * allow_mask, heatmap_t_cuda * allow_mask)/allow_mask.sum().detach()/heatmap.shape[0]/heatmap.shape[1]
-                    loss_paf_total += criterion_paf(paf_out * allow_mask, paf_t_cuda * allow_mask)/allow_mask.sum().detach()/heatmap.shape[0]/paf.shape[1]
+                    loss_hm_total += criterion_hm(heatmap_out * allow_mask, heatmap_t_cuda * allow_mask) \
+                                     / allow_mask.sum().detach()/heatmap.shape[0]/heatmap.shape[1]
+                    loss_paf_total += criterion_paf(paf_out * allow_mask, paf_t_cuda * allow_mask) \
+                                      / allow_mask.sum().detach()/heatmap.shape[0]/paf.shape[1]
                 loss = loss_hm_total + loss_paf_total
 
                 output = (heatmap_outputs[-1].data.cpu().numpy(), paf_outputs[-1].data.cpu().numpy(), indices.numpy())
 
                 if to_train:
-                    optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
 
@@ -58,9 +62,11 @@ class process:
         heatmap_loss_avg, paf_loss_avg = 0.0, 0.0
         for epoch in range(1, n_epochs + 1):
             self.step(train_loader, criterion_hm, criterion_paf, True, optimizer, viz_output=viz_output)
+            torch.cuda.empty_cache()
             if epoch % val_interval == 0:
                 torch.cuda.empty_cache()
-                self.validate_net(test_loader, criterion_hm, criterion_paf, save_dir, epoch, viz_output=viz_output)
+                with torch.no_grad():
+                    self.validate_net(test_loader, criterion_hm, criterion_paf, save_dir, epoch, viz_output=viz_output)
             adjust_learning_rate(optimizer, epoch, drop_lr, learn_rate)
         return heatmap_loss_avg, paf_loss_avg
 
