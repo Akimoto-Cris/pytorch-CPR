f75d1523b7442b4e32b27da821acc75b0e6c7bb5
Akimoto-Cris <29163135+Akimoto-Cris@users.noreply.github.com>
2019-03-24 13:43:52 +0800

    switch argparse experiment scheme to json config

--- src/training/train_net.py
+++ src/training/train_net.py
@@ -3,21 +3,26 @@ from torch.autograd import Variable
 import os
 from tqdm import tqdm
 from visualization.visualize import visualize_output
+import threading
+from queue import Queue
 
-
-# torch.backends.cudnn.enabled = False
-# torch.backends.cudnn.benchmark = False
 
 class process:
     def __init__(self, model):
         self.model = model
 
-
-    def step(self, data_loader, criterion_hm, criterion_paf, to_train=False, optimizer=None, viz_output=False):
+    def step(self, data_loader, criterion_hm, criterion_paf, to_train=False,
+             optimizer=None, viz_output=False, regularization=False, lamda=1.0):
         if to_train:
             self.model.train()
         else:
             self.model.eval()
+
+        if viz_output:
+            self.queue = Queue()
+            self.costumer = visualize_thread(self.queue)
+            self.costumer.start()
+
         nIters = len(data_loader)
         hm_loss_meter, paf_loss_meter = AverageMeter(), AverageMeter()
         with tqdm(total=nIters) as t:
@@ -30,8 +35,8 @@ class process:
                 allow_mask = 1 - ignore_mask_cuda
                 heatmap_outputs, paf_outputs = self.model(input_cuda)
 
-                loss_hm_total = 0
-                loss_paf_total = 0
+                loss_hm_total = 0.0
+                loss_paf_total = 0.0
                 for i in range(len(heatmap_outputs)):
                     heatmap_out = heatmap_outputs[i]
                     paf_out = paf_outputs[i]
@@ -40,15 +45,18 @@ class process:
                     loss_paf_total += criterion_paf(paf_out * allow_mask, paf_t_cuda * allow_mask) \
                                       / allow_mask.sum().detach()/heatmap.shape[0]/paf.shape[1]
                 loss = loss_hm_total + loss_paf_total
-
-                output = (heatmap_outputs[-1].data.cpu().numpy(), paf_outputs[-1].data.cpu().numpy(), indices.numpy())
-
+                if regularization:
+                    loss_reg = 0.0
+                    for param in self.model.parameters():
+                        loss_reg += lamda * torch.norm(param)
+                    loss += loss_reg
+                output = (heatmap_outputs[1].data.cpu().numpy(), paf_outputs[1].data.cpu().numpy(), indices.numpy())
                 if to_train:
                     loss.backward()
                     optimizer.step()
-
                 if viz_output:
-                    visualize_output(input_.numpy(), heatmap.numpy(), paf.numpy(), ignore_mask.numpy(), output)
+                    self.queue.put((input_.numpy(), heatmap.numpy(), paf.numpy(), ignore_mask.numpy(), output))
+
                 hm_loss_meter.update(loss_hm_total.data.cpu().numpy())
                 paf_loss_meter.update(loss_paf_total.data.cpu().numpy())
                 t.set_postfix(loss_hm='{:05.3f}'.format(hm_loss_meter.avg), loss_paf='{:05.3f}'.format(paf_loss_meter.avg))
@@ -56,26 +64,40 @@ class process:
 
         return hm_loss_meter.avg, paf_loss_meter.avg
 
-
     def train_net(self, train_loader, test_loader, criterion_hm, criterion_paf, optimizer,
-                  n_epochs, val_interval, learn_rate, drop_lr, save_dir, viz_output=False):
+                  opt, viz_output=False):
+
         heatmap_loss_avg, paf_loss_avg = 0.0, 0.0
-        for epoch in range(1, n_epochs + 1):
-            self.step(train_loader, criterion_hm, criterion_paf, True, optimizer, viz_output=viz_output)
-            if epoch % val_interval == 0:
+        for epoch in range(1, opt["train"]["nEpoch"] + 1):
+            epoch += opt["env"]["checkpoint"]
+            self.step(train_loader, criterion_hm, criterion_paf, True, optimizer,
+                      regularization=opt["train"]["regularization"], lamda=opt["train"]["lambda"],
+                      viz_output=viz_output)
+            opt["env"]["checkpoint"] = epoch
+            if epoch % opt["train"]["valInterval"] == 0:
                 torch.cuda.empty_cache()
                 with torch.no_grad():
-                    self.validate_net(test_loader, criterion_hm, criterion_paf, save_dir, epoch, viz_output=viz_output)
-            adjust_learning_rate(optimizer, epoch, drop_lr, learn_rate)
-        return heatmap_loss_avg, paf_loss_avg
+                    self.validate_net(test_loader, criterion_hm, criterion_paf, opt["env"]["saveDir"], epoch, viz_output=viz_output)
+            adjust_learning_rate(optimizer, epoch, opt["train"]["dropLR"], opt["train"]["LR"])
 
+        self.costumer.join()
+        return heatmap_loss_avg, paf_loss_avg
 
     def validate_net(self, test_loader, criterion_hm, criterion_paf, save_dir=None, epoch=0, viz_output=False):
         heatmap_loss_avg, paf_loss_avg = self.step(test_loader, criterion_hm, criterion_paf, viz_output=viz_output)
         if save_dir:
-            # torch.save(self.model, os.path.join(save_dir, 'model_{}.pth'.format(epoch)))
             torch.save(self.model.state_dict(), os.path.join(save_dir, 'model_{}.pth'.format(epoch)))
         return heatmap_loss_avg, paf_loss_avg
+
+
+class visualize_thread(threading.Thread):
+    def __init__(self, queue):
+        super(visualize_thread, self).__init__()
+        self.data = queue
+
+    def run(self):
+        input_, heatmap, paf, ignore_mask, output = self.data.get()
+        visualize_output(input_, heatmap, paf, ignore_mask, output)
 
 
 class AverageMeter(object):
