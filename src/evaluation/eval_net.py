0ce6a30d7000138c223e597a6ed62071b03d1736
Akimoto-Cris <29163135+Akimoto-Cris@users.noreply.github.com>
2019-04-30 10:46:47 +0800

    Add files via upload

--- src/evaluation/eval_net.py
+++ src/evaluation/eval_net.py
@@ -1,21 +1,18 @@
 import os
-import scipy.stats
-import matplotlib.pyplot as plt
 import time
-import cv2
 import numpy as np
 import torch, json
 from data_process.process_utils import resize_hm, denormalize
 from visualization.visualize import visualize_output_single
-from .post import plot_pose_pdf
-from openpose_plus.inference.post_process import decode_pose
+from .post import plot_pose_pdf, decode_pose, append_result
+from openpose_plus.inference.post_process import decode_pose as openpose_decode_pose
 
 # Typical evaluation is done on multi-scale and average across all evals is taken as output
 # These reduce the quantization error in the model
 def eval_net(data_loader, model, opts, ids_in_ckpt=[]):
     model.eval()
     dataset = data_loader.dataset
-    scales = [1., 0.5, 0.75, 1.25, 1.5, 2.0]
+    scales = [1., 0.5, 1.25, 1.5, 1.75, 2.0]
     # scales = [1.]
     assert (scales[0]==1)
     n_scales = len(scales)
@@ -23,26 +20,27 @@ def eval_net(data_loader, model, opts, ids_in_ckpt=[]):
     # keypoints_list = []
     runtimes = []
 
-
     with torch.no_grad():
         for i in range(dataset_len):
             if dataset.indices[i] in ids_in_ckpt:
-                print("skip {}th image of image_id {}.".format(i, dataset.indices[i]))
+                print(f"skip {i}th image of image_id {dataset.indices[i]}.")
                 continue
 
             print(i)
             start = time.time()
             # imgs, heatmap_t, paf_t, ignore_mask_t = dataset.get_imgs_multiscale(i, scales, flip=False, to_resize=False)
             if opts["to_test"]:
-                imgs, orig_shape = dataset.get_imgs_multiscale_resize(i, scales, flip=False)
+                imgs, orig_shape = dataset.get_imgs_multiscale_resize(i, scales, flip=False, resize_factor=1)
                 heatmap_t = np.zeros((opts["model"]["nJoints"], opts["test"]["hmSize"], opts["test"]["hmSize"]))
                 paf_t = np.zeros((opts["model"]["nLimbs"], opts["test"]["hmSize"], opts["test"]["hmSize"]))
                 ignore_mask_t = np.zeros((opts["test"]["hmSize"], opts["test"]["hmSize"]))
             else:
-                imgs, heatmap_t, paf_t, ignore_mask_t, orig_shape = dataset.get_imgs_multiscale_resize(i, scales, flip=False)
+                imgs, heatmap_t, paf_t, ignore_mask_t, orig_shape = dataset.get_imgs_multiscale_resize(i, scales,
+                                                                                                       flip=False,
+                                                                                                       resize_factor=1)
             resize_factors = tuple([orig_shape[i] / imgs[0].shape[i + 1] for i in [1, 0]])
             # resize_factors = (1, 1)
-            # print(imgs[0].shape, orig_shape,resize_factors)
+            print(f"input_size:{imgs[0].shape}, origin_size:{orig_shape}")
             n_imgs = len(imgs)
             assert(n_imgs == n_scales)
             heights = list(map(lambda x: x.shape[1], imgs))
@@ -75,10 +73,27 @@ def eval_net(data_loader, model, opts, ids_in_ckpt=[]):
             img_basic = denormalize(img_basic)
             param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
             vis_path = os.path.join(opts["env"]["saveDir"], "val" if not opts["to_test"] else "test" + '_viz')
+
+            def native():
+                candidates, subset = decode_pose(img_basic, param, heatmap_avg, paf_avg)
+                plot_pose_pdf(img_basic, candidates, subset, os.path.join(vis_path, f"{i}.pdf"))
+                outputs = append_result(dataset.indices[i], subset, candidates)
+                return outputs
+
+            def openpose_inf():
+                outputs = openpose_decode_pose(img_basic, param, heatmap_avg, paf_avg, opts,
+                                               dataset.indices[i], os.path.join(vis_path, f"{i}.pdf"),
+                                               resize_shape=None)
+                return outputs
+
             if not os.path.exists(vis_path):
                 os.makedirs(vis_path)
-            outputs = decode_pose(img_basic, param, heatmap_avg, paf_avg, opts, dataset.indices[i], os.path.join(vis_path, f"{i}.pdf"))
+
+            outputs = native() # native()
+            for out in outputs:
+                out['keypoints'][::3] = [i * resize_factors[0] for i in out['keypoints'][::3]]
+                out['keypoints'][1::3] = [i * resize_factors[1] for i in out['keypoints'][1::3]]
             final = time.time()-start
             print("both loops took ", final)
 
-            yield outputs, i
+            yield outputs, dataset.indices[i]
